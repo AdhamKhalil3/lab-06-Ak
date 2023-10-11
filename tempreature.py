8b3ef21958861780d05fdb558ffa599d252a1c17
adham khalil <adhamkhalil@Adhams-MBP.lan>
2023-10-10 21:19:21 -0400

    my shout file

--- tempreature.py
+++ tempreature.py
@@ -1,4 +1,4 @@
 tempincelsius = input( "please enter a temperature in celsius")
 tempinfahrenheit = float((tempincelsius) * 1.8) + 32
 print("The temperature is " + str (tempinfahrenheit) + " degrees F)"The temperature is " + tempinfahrenheit + " degrees F)"
-   
+   ``
